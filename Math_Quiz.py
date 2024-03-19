ce5d13185e1e5e1f18240728f2d81e3e8ba94431
VincentMoses123 <24.vincent.moses@gmail.com>
2024-03-18 20:53:35 -0400

    Final commit, now with comments!

--- Math_Quiz.py
+++ Math_Quiz.py
@@ -7,16 +7,16 @@ MAX_OPERAND = 12 #Highest Number you can get
 TOTAL_PROBLEMS = 5 #Number of problems
 
 def generate_problem():
-    left = random.randint(MIN_OPERAND, MAX_OPERAND)
-    right = random.randint(MIN_OPERAND, MAX_OPERAND)
-    operator = random.choice(OPERATORS)
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)#----------------------------------------------
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)# This gives us the random operator and number 
+    operator = random.choice(OPERATORS)#----------------------------------------------------------
     
     expr = str(left) + " " + operator + " " + str(right)
     answer = eval(expr)
     return expr, answer
 
 wrong = 0
-input("Press enter to start!")
+input("Test your math skills with this timed quiz! Press enter to start!")
 print("---------------------")
 
 start_time = time.time()
@@ -30,7 +30,7 @@ for i in range(TOTAL_PROBLEMS):
         wrong += 1
 
 end_time = time.time()
-total_time = end_time - start_time        
+total_time = end_time - start_time #Gives time at end of code   
 
 print("---------------------")
 print("Nice work! You finished in", total_time, "seconds!")
