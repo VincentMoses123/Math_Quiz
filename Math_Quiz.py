e8ddd13417a06cd0eff34cfe0e15991042ffa851
VincentMoses123 <24.vincent.moses@gmail.com>
2024-03-18 20:37:58 -0400

    2nd Commit

--- Math_Quiz.py
+++ Math_Quiz.py
@@ -1,18 +1,24 @@
 import random
 
-operators = ["+", "-", "*"]
+OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 5
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
-    operator = random.choice(operators)
+    operator = random.choice(OPERATORS)
     
     expr = str(left) + " " + operator + " " + str(right)
-    print(expr)
-    return expr
+    answer = eval(expr)
+    return expr, answer
 
-generate_problem()
-    
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ") 
+        if guess == str(answer):
+            break
+
     
